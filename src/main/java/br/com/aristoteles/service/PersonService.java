baacb76f99aba36bcf38a61dcfc9f455211fe36d
aristotelesbr <aristotelesbr@gmail.com>
2023-10-09 18:08:18 -0300

    feat(person): creation person with success

--- src/main/java/br/com/aristoteles/service/PersonService.java
+++ src/main/java/br/com/aristoteles/service/PersonService.java
@@ -2,9 +2,19 @@ package br.com.aristoteles.service;
 
 import br.com.aristoteles.model.Person;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+
 public class PersonService implements IPersonService {
   @Override
   public Person createPerson(Person person) {
+    if (person.getEmail() == null || person.getEmail().isBlank()) {
+      throw new IllegalArgumentException("Email is required!");
+    }
+
+    var id = new AtomicLong().incrementAndGet();
+    person.setId(id);
+
     System.out.println("Person created!");
     return person;
   }
